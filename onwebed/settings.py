4bdaf07b69eaf061a00b1706b558da7d0b2ea1d0
Nahiyan Alamgir <alnahiyan97@gmail.com>
2019-03-31 18:13:47 +0600

    Updated stuff.

--- onwebed/settings.py
+++ onwebed/settings.py
@@ -1,3 +1,5 @@
+import django_heroku
+
 """
 Django settings for onwebed project.
 
@@ -124,3 +126,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+django_heroku.settings(locals())
