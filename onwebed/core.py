ccd0413c396c2984ecba456c59c9eadc37679afe
towniyan <towni.eyan@gmail.com>
2019-05-02 12:08:26 +0600

    Removed singleton stuff.

--- onwebed/core.py
+++ onwebed/core.py
@@ -3,121 +3,108 @@ from django.core.files.storage import default_storage
 from pages.models import Page
 import re
 
-class CoreSingleton:
-    instance = None
 
-    @staticmethod
-    def getInstance():
-        if (CoreSingleton.instance == None):
-            CoreSingleton.instance = CoreSingleton();
+def unescape(string):
+    return \
+        string \
+            .encode("utf-8") \
+            .decode('unicode_escape')
 
-        return CoreSingleton.instance;
+def write_template_file(page_name, page_content):
+    path = \
+        "pages/templates/pages/cached/" \
+            + page_name \
+            + ".html"
+            
+    if default_storage.exists(path):
+        default_storage.delete(path)
 
-
-    def unescape(self, string):
-        return \
-            string \
-                .encode("utf-8") \
-                .decode('unicode_escape')
-
-    def write_template_file(self, page_name, page_content):
-        path = \
-            "pages/templates/pages/cached/" \
-                + page_name \
-                + ".html"
-                
-        if default_storage.exists(path):
-            default_storage.delete(path)
-
-        default_storage.save(
-            path
-            , ContentFile(page_content)
-        )
+    default_storage.save(
+        path
+        , ContentFile(page_content)
+    )
 
 
-    def page_name(self, page_tag):
-        return page_tag[10:-3].strip()
+def page_name(page_tag):
+    return page_tag[10:-3].strip()
 
 
-    def fill_name(self, fill_tag):
-        return fill_tag[10:-2].strip()
+def fill_name(fill_tag):
+    return fill_tag[10:-2].strip()
 
 
-    def hole_name(self, hole_tag):
-        return hole_tag[10:-2].strip()
+def hole_name(hole_tag):
+    return hole_tag[10:-2].strip()
 
 
-    def process_content(self, content):
-        # page inclusions
+def process_content(content):
+    # page inclusions
 
-        page_tags = re.findall("<page id=\"[^\"]+\"\/>", content)
+    page_tags = re.findall("<page id=\"[^\"]+\"\/>", content)
 
-        page_names = []
-        for page_tag in page_tags:
-            page_names.append(self.page_name(page_tag))
+    page_names = []
+    for page_tag in page_tags:
+        page_names.append(page_name(page_tag))
 
-        pages = Page.objects.filter(name__in = page_names)
+    pages = Page.objects.filter(name__in = page_names)
 
-        for page in pages:
-            content = \
-                re.sub("<page id=\"" + page.name + "\"/>"
-                    , self.process_content(page.html_content)
-                    , content)
+    for page in pages:
+        content = \
+            re.sub("<page id=\"" + page.name + "\"/>"
+                , process_content(page.html_content)
+                , content)
 
+    
+    # fills
+
+    fill_tags = re.findall("<fill id=\"[^\"]+\">", content)
+
+    fills = []
+    # fill_names, fill_contents = [], []
+    for fill_tag in fill_tags:
+        pos = content.find("<fill id=\"" + fill_name(fill_tag) + "\">")
+        length = len("<fill id=\"" + fill_name(fill_tag) + "\">")
         
-        # fills
+        pos2 = content.find("</fill>", pos + length)
 
-        fill_tags = re.findall("<fill id=\"[^\"]+\">", content)
+        fills.append({
+            'name': fill_name(fill_tag),
+            'content': content[(pos + length):pos2]
+        })
 
-        fills = []
-        for fill_tag in fill_tags:
-            pos = content.find("<fill id=\"" + self.fill_name(fill_tag) + "\">")
-            length = len("<fill id=\"" + self.fill_name(fill_tag) + "\">")
-            
-            pos2 = content.find("</fill>", pos + length)
+        content = content[:pos] + content[(pos2 + len("</fill>")):]
 
-            fills.append({
-                'name': self.fill_name(fill_tag),
-                'content': content[(pos + length):pos2]
-            })
+    # holes
 
-            content = content[:pos] + content[(pos2 + len("</fill>")):]
+    hole_tags = re.findall("<hole id=\"[^\"]+\">", content)
 
-        # holes
+    for hole_tag in hole_tags:
+        pos = content.find("<hole id=\"" + hole_name(hole_tag) + "\">")
+        length = len("<hole id=\"" + hole_name(hole_tag) + "\">")
+        
+        pos2 = content.find("</hole>", pos + length)
 
-        hole_tags = re.findall("<hole id=\"[^\"]+\">", content)
+        for fill in fills:
+            if hole_name(hole_tag) == fill['name']:
+                content = content[:pos] + fill['content'] + content[(pos2 + len("</hole>")):]
 
-        for hole_tag in hole_tags:
-            pos = content.find("<hole id=\"" + self.hole_name(hole_tag) + "\">")
-            length = len("<hole id=\"" + self.hole_name(hole_tag) + "\">")
-            
-            pos2 = content.find("</hole>", pos + length)
-
-            for fill in fills:
-                if self.hole_name(hole_tag) == fill['name']:
-                    content = content[:pos] + fill['content'] + content[(pos2 + len("</hole>")):]
-
-        return content
+    return content
 
 
-    def remove_holes(self, content):
-        hole_tags = re.findall("<hole id=\"[^\"]+\">", content)
+def remove_holes(content):
+    hole_tags = re.findall("<hole id=\"[^\"]+\">", content)
 
-        for hole_tag in hole_tags:
-            pos = content.find("<hole id=\"" + self.hole_name(hole_tag) + "\">")
-            length = len("<hole id=\"" + self.hole_name(hole_tag) + "\">")
-            
-            pos2 = content.find("</hole>", pos + length)
+    for hole_tag in hole_tags:
+        pos = content.find("<hole id=\"" + hole_name(hole_tag) + "\">")
+        length = len("<hole id=\"" + hole_name(hole_tag) + "\">")
+        
+        pos2 = content.find("</hole>", pos + length)
 
-            content = content[:pos] + content[(pos + length):pos2] + content[(pos2 + len("</hole>")):]
+        content = content[:pos] + content[(pos + length):pos2] + content[(pos2 + len("</hole>")):]
 
-        return content
+    return content
 
 
-    def cache_page(self, page_name, content):
-        self.write_template_file(
-            page_name,
-            self.remove_holes(
-                self.process_content(
-                    content)))
-        Page.objects.filter(name = page_name).update(is_cached = True)
+def cache_page(page_name, content):
+    write_template_file(page_name, remove_holes(process_content(content)))
+    Page.objects.filter(name = page_name).update(is_cached = True)
--- pages/views.py
+++ pages/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Page
-from onwebed.core import CoreSingleton
+from onwebed import core
 
 # Create your views here.
 
@@ -38,14 +38,14 @@ def edit(request, page_id):
 		page.content = request.POST["content"]
 		page.name = request.POST["name"]
 		page.title = request.POST["title"]
-		page.html_content = CoreSingleton.getInstance().unescape(request.POST["template_content"])
+		page.html_content = core.unescape(request.POST["template_content"])
 		page.save()
 
 		# expire all page caches
 		Page.objects.all().update(is_cached = False)
 
 		# write the template file
-		CoreSingleton.getInstance().cache_page(page.name, page.html_content)
+		core.cache_page(page.name, page.html_content)
 		
 		# messages.success(request, 'Page saved successfully!')
 
@@ -79,7 +79,7 @@ def detail(request, page_id):
 	}
 
 	if not page.is_cached:
-		CoreSingleton.getInstance().cache_page(page.name, page.html_content)
+		core.cache_page(page.name, page.html_content)
 
 	return render(request, "pages/cached/" + page.name + ".html", context)
 
@@ -93,6 +93,6 @@ def detail_by_name(request, page_name):
 	}
 
 	if not page.is_cached:
-		CoreSingleton.getInstance().cache_page(page.name, page.html_content)
+		core.cache_page(page.name, page.html_content)
 
 	return render(request, "pages/cached/" + page.name + ".html", context)
