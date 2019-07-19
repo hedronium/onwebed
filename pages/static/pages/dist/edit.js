ed5325ac4137f0705b06231b7a50e0e2d0d14bdc
nahiyan <nahiyan.alamgir@northsouth.edu>
2019-07-19 13:16:48 +0600

    Fix responsive issues.

--- pages/static/pages/dist/edit.js
+++ pages/static/pages/dist/edit.js
@@ -14204,7 +14204,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53710" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58467" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
